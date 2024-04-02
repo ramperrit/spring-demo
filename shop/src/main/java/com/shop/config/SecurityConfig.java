d9652844f08832aa7799452ca67b50ada282b3ca
ramperrit <153475173+ramperrit@users.noreply.github.com>
2024-04-02 17:51:17 +0900

    2024.04.02

--- shop/src/main/java/com/shop/config/SecurityConfig.java
+++ shop/src/main/java/com/shop/config/SecurityConfig.java
@@ -2,11 +2,16 @@ package com.shop.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -17,8 +22,42 @@ public class SecurityConfig{
         return new BCryptPasswordEncoder();
     }
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-//
-//    }
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.formLogin((it) -> it
+                .loginPage("/members/login")
+                .defaultSuccessUrl("/")
+                .usernameParameter("email")
+                .failureUrl("/members/login/error")
+        );
+
+        http.logout((it) -> it
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                .logoutSuccessUrl("/")
+        );
+
+        http.authorizeHttpRequests((req)-> {req
+                .requestMatchers(antMatcher("/")).permitAll()
+                .requestMatchers(antMatcher("/members/**")).permitAll()
+                .requestMatchers(antMatcher("/item/**")).permitAll()
+                .requestMatchers(antMatcher("/css/**")).permitAll()
+                .requestMatchers(antMatcher("/images/**")).permitAll()
+                .requestMatchers(antMatcher("/admin/**")).hasRole("ADMIN")
+                .anyRequest().authenticated();
+        });
+
+        http.exceptionHandling((e) -> e.authenticationEntryPoint(new CustomAuthenticationEntryPoint()));
+
+        return http.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+
+
+
+
 }
--- shop/src/main/java/com/shop/dto/MemberFormDto.java
+++ shop/src/main/java/com/shop/dto/MemberFormDto.java
@@ -1,13 +1,27 @@
 package com.shop.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Getter
 @Setter
 public class MemberFormDto {
+
+    @NotBlank(message = "이름 입력 필수")
     private String name;
+
+    @NotEmpty(message = "이메일 입력 필수")
+    @Email(message = "이메일 형식으로 작성")
     private String email;
+
+    @NotEmpty(message = "비밀번호 입력 필수")
+    @Length(min=8, max=16, message = "8~16 자리")
     private String password;
+
+    @NotEmpty(message = "주소 입력 필수")
     private String address;
 }
