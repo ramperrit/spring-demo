8ba4138724cc45941abd2d58ff92368308fc917f
ramperrit <153475173+ramperrit@users.noreply.github.com>
2024-04-16 15:42:15 +0900

    aa

--- src/main/java/com/shop/service/ItemImgService.java
+++ src/main/java/com/shop/service/ItemImgService.java
@@ -14,7 +14,7 @@ import org.thymeleaf.util.StringUtils;
 @RequiredArgsConstructor
 @Transactional
 public class ItemImgService {
-    @Value("home/ec2-user/project/spring-demo/target/itemImg")
+    @Value("C:/shop/item")
     private String itemImgLoction;
 
     private final ItemImgRepository itemImgRepository;
